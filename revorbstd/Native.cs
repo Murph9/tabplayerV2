f9644aa9b9da4b1d8e9ef75f0da6700b7373d8ae
Jake Murphy <murph9y@gmail.com>
2023-09-09 20:03:53 +1000

    fix assembly location not found -> can't find librevorb library files

--- revorbstd/Native.cs
+++ revorbstd/Native.cs
@@ -1,18 +1,34 @@
 ﻿using System;
+using System.IO;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace RevorbStd {
     public class Native {
+
+        // This file has been modified to work with a c# code solution which doesn't have a native assembly location:
+        // https://stackoverflow.com/questions/48589883/assembly-codebase-gives-wrong-location
+
+        // Godot c# seems to load the dlls via Assembly.Load(byte[]) which prevents the use of Assembly.GetExecutingAssembly() location
+        // So we just do a manual search for it...
+
         //required for hassle-free native lib loading on linux (without it user must have .so
         //libs installed in /libs/ or datatool path defined in LD_LIBRARY_PATH env var)
         private static IntPtr SharedLibraryResolver(string libraryName, Assembly assembly, DllImportSearchPath? p) {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                return NativeLibrary.Load("../revorbstd/librevorb.dll", assembly, DllImportSearchPath.AssemblyDirectory);
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+                if (File.Exists("../revorbstd/librevorb.dll"))
+                    return NativeLibrary.Load("../revorbstd/librevorb.dll", assembly, DllImportSearchPath.AssemblyDirectory);
+                
+                return NativeLibrary.Load("librevorb.dll", assembly, DllImportSearchPath.AssemblyDirectory);
+            }
 
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                return NativeLibrary.Load("../revorbstd/librevorb.so", assembly, DllImportSearchPath.AssemblyDirectory);
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
+                if (File.Exists("../revorbstd/librevorb.so"))
+                    return NativeLibrary.Load("../revorbstd/librevorb.so", assembly, DllImportSearchPath.AssemblyDirectory);
+
+                return NativeLibrary.Load("librevorb.so", assembly, DllImportSearchPath.AssemblyDirectory);
+            }
 
             Console.WriteLine("Current platform doesn't support librevorb. Sound conversion to .ogg is not available.");
             return IntPtr.Zero;
