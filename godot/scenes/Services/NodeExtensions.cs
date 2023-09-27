a827f154ce4b881bff2dd600aea4a5692648f170
Jake Murphy <murph9y@gmail.com>
2023-09-27 19:28:33 +1000

    move node extensions and give namespace

--- godot/scenes/Services/NodeExtensions.cs
+++ godot/scenes/Services/NodeExtensions.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using Godot;
 
+namespace murph9.TabPlayer.scenes.Services;
+
 public static class NodeExtensions {
 
     public static void AddChildren(this Node node, IEnumerable<Node> nodes) {
