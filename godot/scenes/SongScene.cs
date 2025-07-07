26c7f92b7e37dcea55f58b859b3db17f61b8273f
murph9 <murph9y@gmail.com>
2025-07-07 19:52:13 +1000

    Allow songs to be sped up and slowed down for learning

--- godot/scenes/SongScene.cs
+++ godot/scenes/SongScene.cs
@@ -9,218 +9,239 @@ using System.Linq;
 namespace murph9.TabPlayer.scenes;
 
 public interface IAudioStreamPosition {
-	public bool SongPlaying { get; }
-	public double GetSongPosition();
+    public bool SongPlaying { get; }
+    public double GetSongPosition();
 }
 
-public partial class SongScene : Node, IAudioStreamPosition
-{
-	private SongState _state;
-	private AudioStreamOggVorbis _audioStream;
-	private AudioStreamPlayer _player;
-	private double? _cachedSongPosition;
+public partial class SongScene : Node, IAudioStreamPosition {
 
-	public bool SongPlaying => _player?.Playing == true;
+    private const float SONG_SPEED_DIFF = 0.98f;
+
+    private SongState _state;
+    private AudioStreamOggVorbis _audioStream;
+    private AudioStreamPlayer _player;
+    private double? _cachedSongPosition;
+
+    public bool SongPlaying => _player?.Playing == true;
 
     [Signal]
-	public delegate void ClosedEventHandler();
+    public delegate void ClosedEventHandler();
 
-	public void _init(SongState state) {
-		_state = state;
-		_audioStream = AudioStreamOggVorbis.LoadFromBuffer(state.Audio);
-	}
+    public void _init(SongState state) {
+        _state = state;
+        _audioStream = AudioStreamOggVorbis.LoadFromBuffer(state.Audio);
+    }
 
-	private void SongFinished() {
-		_player.Play();
-		_player.Seek(0);
-		_player.StreamPaused = true;
-		Pause();
-	}
+    private void SongFinished() {
+        _player.Play();
+        _player.Seek(0);
+        _player.StreamPaused = true;
+        Pause();
+    }
 
-	private void Pause() {
-		_player.StreamPaused = true;
-		
-		var pause = GetNode<Window>("PauseWindow");
-		pause.PopupCentered();
-		pause.ChildControlsChanged();
-	}
+    private void Pause() {
+        _player.StreamPaused = true;
+        
+        var pause = GetNode<Window>("PauseWindow");
+        pause.PopupCentered();
+        pause.ChildControlsChanged();
+    }
 
-	private void Resume() {
-		_player.StreamPaused = false;
-		
-		var pause = GetNode<Window>("PauseWindow");
-		if (pause.Visible)
-			pause.Hide();
-	}
+    private void Resume() {
+        _player.StreamPaused = false;
+        
+        var pause = GetNode<Window>("PauseWindow");
+        if (pause.Visible)
+            pause.Hide();
+    }
 
-	private void Quit() {
-		_player.Stop();
-		Pause(); // prevent errors in final update frame
-		
-		EmitSignal(SignalName.Closed);
-	}
+    private void Quit() {
+        _player.Stop();
+        Pause(); // prevent errors in final update frame
+        
+        EmitSignal(SignalName.Closed);
+    }
 
-	private void PauseWindow_Input(InputEvent @event) {
-		_Input(@event);
-	}
+    private void PauseWindow_Input(InputEvent @event) {
+        _Input(@event);
+    }
 
-	public override void _Input(InputEvent @event) {
-		if (@event.IsActionPressed("ui_cancel") || @event.IsActionPressed("song_pause"))
-		{
-			var pause = GetNode<Window>("PauseWindow");
-			if (!pause.Visible) {
-				Pause();
-			} else {
-				Resume();
-			}
-		}
+    public override void _Input(InputEvent @event) {
+        if (@event.IsActionPressed("ui_cancel") || @event.IsActionPressed("song_pause")) {
+            var pause = GetNode<Window>("PauseWindow");
+            if (!pause.Visible) {
+                Pause();
+            } else {
+                Resume();
+            }
+        }
 
-		if (@event.IsActionPressed("song_skip_forward_10")) {
-			Skip10Sec();
-		} else if (@event.IsActionPressed("song_skip_backward_10")) {
-			Back10Sec();
-		} else if (@event.IsActionPressed("song_skip_to_next")) {
-			SkipToNext();
-		} else if (@event.IsActionPressed("song_restart")) {
-			RestartSong();
-		}
-	}
+        if (@event.IsActionPressed("song_skip_forward_10")) {
+            Skip10Sec();
+        } else if (@event.IsActionPressed("song_skip_backward_10")) {
+            Back10Sec();
+        } else if (@event.IsActionPressed("song_skip_to_next")) {
+            SkipToNext();
+        } else if (@event.IsActionPressed("song_restart")) {
+            RestartSong();
+        } else if (@event.IsActionPressed("song_speed_down")) {
+            SlowDownPlayback();
+        } else if (@event.IsActionPressed("song_speed_up")) {
+            SpeedUpPlayback();
+        }
+    }
 
-	private void Skip10Sec() => _player.Seek((float)GetSongPosition() + 10f);
-	private void Back10Sec() => _player.Seek((float)GetSongPosition() - 10f);
-	private void SkipToNext() {
-		var nextNote = NextNoteBlock();
-		if (nextNote == null)
-			return;
-		_player.Seek(nextNote.Time - 1.5f);
-	}
-	private void RestartSong() => _player.Seek(0);
+    private void Skip10Sec() => _player.Seek((float)GetSongPosition() + 10f);
+    private void Back10Sec() => _player.Seek((float)GetSongPosition() - 10f);
+    private void SkipToNext() {
+        var nextNote = NextNoteBlock();
+        if (nextNote == null)
+            return;
+        _player.Seek(nextNote.Time - 1.5f);
+    }
+    private void RestartSong() => _player.Seek(0);
 
-	public override void _Ready()
-	{
-		var info = _state.SongInfo;
-		SetUILabels(info);
+    private void SlowDownPlayback() => AdjustPitch(SONG_SPEED_DIFF);
+    private void SpeedUpPlayback() => AdjustPitch(1f / SONG_SPEED_DIFF);
 
-		_player = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
-		_player.Stream = _audioStream;
-		//_player.VolumeDb = 0;
-		//_player.Bus = "some bus name with phase shift"
-		_player.Play();
-		
-		var guitarChartScene = GD.Load<CSharpScript>("res://scenes/song/GuitarChart.cs").New().As<GuitarChart>();
-		guitarChartScene._init(_state, this);
-		AddChild(guitarChartScene);
+    private void AdjustPitch(float amount) {
+        if (_player.PitchScale < 0.5f && amount < 1)
+            return;
+        if (_player.PitchScale > 1.7f && amount > 1)
+            return;
 
-		var noteGraphScene = GD.Load<CSharpScript>("res://scenes/song/NoteGraph.cs").New().As<NoteGraph>();
-		noteGraphScene._init(_state, this);
-		AddChild(noteGraphScene);
+        _player.PitchScale *= amount;
 
-		try {
-			var songScene = GD.Load<CSharpScript>("res://scenes/song/SongChart.cs").New().As<SongChart>();
-			songScene._init(_state.Instrument);
-			AddChild(songScene);
-		} catch (Exception e) {
-			GD.Print(e);
-		}
-	}
+        var busId = AudioServer.GetBusIndex("SongPlayback");
+        var effect = AudioServer.GetBusEffect(busId, 0) as AudioEffectPitchShift;
+        effect.PitchScale = 1/_player.PitchScale;
+    }
 
-	public override void _Process(double delta)
-	{
-		_cachedSongPosition = null;
-		if (!_player.Playing)
-			return;
+    public override void _Ready() {
+        var info = _state.SongInfo;
+        SetUILabels(info);
 
-		var nextNote = NextNoteBlock();
+        _player = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
+        _player.Stream = _audioStream;
+        //_player.VolumeDb = 0;
+        _player.Bus = "SongPlayback";
+        _player.Play();
 
-		var noteText = (nextNote == null) ? "No note" : "Next: " + Math.Round(nextNote.Time, 3) + " in " + Math.Round(nextNote.Time - GetSongPosition(), 1);
+        var guitarChartScene = GD.Load<CSharpScript>("res://scenes/song/GuitarChart.cs").New().As<GuitarChart>();
+        guitarChartScene._init(_state, this);
+        AddChild(guitarChartScene);
 
-		var debugText = @$"{noteText}
+        var noteGraphScene = GD.Load<CSharpScript>("res://scenes/song/NoteGraph.cs").New().As<NoteGraph>();
+        noteGraphScene._init(_state, this);
+        AddChild(noteGraphScene);
+
+        try {
+            var songScene = GD.Load<CSharpScript>("res://scenes/song/SongChart.cs").New().As<SongChart>();
+            songScene._init(_state.Instrument);
+            AddChild(songScene);
+        } catch (Exception e) {
+            GD.Print(e);
+        }
+    }
+
+    public override void _Process(double delta)
+    {
+        _cachedSongPosition = null;
+        if (!_player.Playing)
+            return;
+
+        var nextNote = NextNoteBlock();
+
+        var noteText = (nextNote == null) ? "No note" : "Next: " + Math.Round(nextNote.Time, 3) + " in " + Math.Round(nextNote.Time - GetSongPosition(), 1);
+
+        var debugText = @$"Song Speed: {Mathf.Round(_player.PitchScale * 100)}%
+{noteText}
 {Engine.GetFramesPerSecond()}fps | {delta*1000:000.0}ms
 {GetSongPosition().ToMinSec(true)}
 ";
-		GetNode<Label>("RunningDetailsLabel").Text = debugText;
+        GetNode<Label>("RunningDetailsLabel").Text = debugText;
 
-		UpdateLyrics(GetNode<RichTextLabel>("HBoxContainer/LyricsLabel"));
-	}
-
-	public double GetSongPosition()
-    {
-		if (_cachedSongPosition.HasValue) {
-			return _cachedSongPosition.Value;
-		}
-
-		double time = _player.GetPlaybackPosition() + AudioServer.GetTimeSinceLastMix();
-		time -= AudioServer.GetOutputLatency();
-		_cachedSongPosition = time;
-		return time;
-
-		// TODO this might have some stuttering which we'll need to fix
+        UpdateLyrics(GetNode<RichTextLabel>("HBoxContainer/LyricsLabel"));
     }
 
-	private void SetUILabels(SongInfo info) {
-		var infoLabel = GetNode<Label>("SongInfoLabel");
-		infoLabel.Text = $"{info.Metadata.Name} ({info.Metadata.Year})\n{info.Metadata.Artist}";
+    public double GetSongPosition()
+    {
+        if (_cachedSongPosition.HasValue) {
+            return _cachedSongPosition.Value;
+        }
 
-		var detailsLabel = GetNode<Label>("SongDetailsLabel");
-		var guitarTuning = "Tuning: " + Instrument.CalcTuningName(_state.Instrument.Config.Tuning, _state.Instrument.Config.CapoFret);
-		detailsLabel.Text = $@"Playing: {_state.Instrument.Name}
+        double time = _player.GetPlaybackPosition() + AudioServer.GetTimeSinceLastMix();
+        time -= AudioServer.GetOutputLatency();
+        _cachedSongPosition = time;
+        return time;
+
+        // TODO this might have some stuttering which we'll need to fix
+    }
+
+    private void SetUILabels(SongInfo info) {
+        var infoLabel = GetNode<Label>("SongInfoLabel");
+        infoLabel.Text = $"{info.Metadata.Name} ({info.Metadata.Year})\n{info.Metadata.Artist}";
+
+        var detailsLabel = GetNode<Label>("SongDetailsLabel");
+        var guitarTuning = "Tuning: " + Instrument.CalcTuningName(_state.Instrument.Config.Tuning, _state.Instrument.Config.CapoFret);
+        detailsLabel.Text = $@"Playing: {_state.Instrument.Name}
 Notes: {_state.Instrument.SingleNoteCount()}
 Chords: {_state.Instrument.ChordCount()}
 {guitarTuning}
 First note @ {_state.Instrument.Notes.First().Time.ToMinSec()}
 Last note @ {_state.Instrument.Notes.Last().Time.ToMinSec()}";
-	}
+    }
 
-	private NoteBlock NextNoteBlock() {
-		var songPos = GetSongPosition();
-		foreach (var b in _state.Instrument.Notes) {
-			if (b.Time > songPos)
-				return b;
-		}
-		return null;
-	}
+    private NoteBlock NextNoteBlock() {
+        var songPos = GetSongPosition();
+        foreach (var b in _state.Instrument.Notes) {
+            if (b.Time > songPos)
+                return b;
+        }
+        return null;
+    }
 
-	private void UpdateLyrics(RichTextLabel label) {
-		label.Clear();
-		label.PushFontSize(40);
-		
-		//TODO don't move to the next lyrics until the next set starts
-		var songPos = GetSongPosition();
-		var curList = GetCurLines(_state, songPos);
-		if (curList.Length < 1) {
-			label.Text = null;
-			return;
-		}
+    private void UpdateLyrics(RichTextLabel label) {
+        label.Clear();
+        label.PushFontSize(40);
 
-		if (curList[0] != null) {
-			var (partA, partB) = curList[0].GetParts(songPos);
-			label.PushColor(Colors.Red);
-			label.AddText(partA);
-			label.PushColor(Colors.Yellow);
-			label.AddText(partB);
-		}
+        //TODO don't move to the next lyrics until the next set starts
+        var songPos = GetSongPosition();
+        var curList = GetCurLines(_state, songPos);
+        if (curList.Length < 1) {
+            label.Text = null;
+            return;
+        }
 
-		if (curList.Length > 1) {
-			label.AddText("\n" + curList[1]?.ToString());
-		}
-	}
+        if (curList[0] != null) {
+            var (partA, partB) = curList[0].GetParts(songPos);
+            label.PushColor(Colors.Red);
+            label.AddText(partA);
+            label.PushColor(Colors.Yellow);
+            label.AddText(partB);
+        }
 
-	private static LyricLine[] GetCurLines(SongState state, double songPos) {
-		var lyrics = state.SongInfo.Lyrics;
-		if (lyrics == null || !lyrics.Lines.Any())
-			return Array.Empty<LyricLine>();
+        if (curList.Length > 1) {
+            label.AddText("\n" + curList[1]?.ToString());
+        }
+    }
 
-		if (lyrics.Lines.First().StartTime > songPos) {
-			// its the first one time
-			return lyrics.Lines.Take(2).ToArray();
-		}
+    private static LyricLine[] GetCurLines(SongState state, double songPos) {
+        var lyrics = state.SongInfo.Lyrics;
+        if (lyrics == null || !lyrics.Lines.Any())
+            return Array.Empty<LyricLine>();
 
-		foreach (var s in lyrics.Lines) {
-			if (s.EndTime < songPos)
-				continue;
-			return lyrics.Lines.SkipWhile(x => x != s).Take(2).ToArray();
-		}
+        if (lyrics.Lines.First().StartTime > songPos) {
+            // its the first one time
+            return lyrics.Lines.Take(2).ToArray();
+        }
 
-		return Array.Empty<LyricLine>();
-	}
+        foreach (var s in lyrics.Lines) {
+            if (s.EndTime < songPos)
+                continue;
+            return lyrics.Lines.SkipWhile(x => x != s).Take(2).ToArray();
+        }
+
+        return Array.Empty<LyricLine>();
+    }
 }
