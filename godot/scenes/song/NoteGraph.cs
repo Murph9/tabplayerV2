db9b4fa7900adf210a4ff46e8c46d4292705349f
murph9 <murph9y@gmail.com>
2025-07-08 22:26:09 +1000

    update the note graph to show the current position and a rough 3 notes per second line

--- godot/scenes/song/NoteGraph.cs
+++ godot/scenes/song/NoteGraph.cs
@@ -8,11 +8,12 @@ using System.Linq;
 namespace murph9.TabPlayer.scenes.song;
 
 public partial class NoteGraph : Node {
-    
-    private const float BUCKET_SIZE = 3;
-    private const float BUCKET_PIXEL_WIDTH = 7;
-    private const float BUCKET_NOTE_PIXEL_HEIGHT = 5;
-    private const float BUCKET_BOTTOM_OFFSET = 20;
+
+    private const float BUCKET_SIZE = 3; // seconds
+    private const float BUCKET_PIXEL_WIDTH = 7; // px
+    private const float BUCKET_NOTE_PIXEL_HEIGHT = 5; // px
+    private const float BUCKET_BOTTOM_OFFSET = 20; // px
+    private const float BUCKETS_RIGHT_SIDE_OFFSET = 3; // in buckets
 
     partial class GraphSection : Control {
         public Vector2 Pos;
@@ -43,8 +44,10 @@ public partial class NoteGraph : Node {
 
     private SongState _songState;
     private IAudioStreamPosition _audio;
+    private Line2D _currentPostionLine;
+    private int _bucketCount;
 
-    private readonly Dictionary<int, GraphSection> _graphBars = new();
+    private readonly Dictionary<int, GraphSection> _graphBars = [];
     // TODO show which strings each section has
 
     public void _init(SongState songState, IAudioStreamPosition audio) {
@@ -52,8 +55,7 @@ public partial class NoteGraph : Node {
         _audio = audio;
     }
 
-	public override void _Ready()
-	{
+    public override void _Ready() {
         var barNode = new Control() {
             Name = "BarNode",
             LayoutMode = 3,
@@ -63,30 +65,46 @@ public partial class NoteGraph : Node {
 
         var stringColours = SettingsService.Settings().StringColours.ToArray();
         var stringOrder = Enumerable.Range(0, 6);
-            if (SettingsService.Settings().LowStringIsLow)
-                stringOrder = stringOrder.Reverse();
+        if (SettingsService.Settings().LowStringIsLow)
+            stringOrder = stringOrder.Reverse();
 
         // TODO align to section starts
-        var noteBuckets = _songState.Instrument.Notes.ToLookup(x => Math.Round(x.Time/BUCKET_SIZE));
-        var maxBucket = Math.Round(noteBuckets.Last().Key + 3);
-        for (var i = 0; i < maxBucket; i++) {
+        var noteBuckets = _songState.Instrument.Notes.ToLookup(x => Math.Round(x.Time / BUCKET_SIZE));
+        _bucketCount = (int)Math.Round(noteBuckets.Last().Key);
+
+        // draw a single note rate reference line
+        // TODO align to absolute start and end of the song
+        var rateLine = new Line2D() {
+            DefaultColor = Colors.Gray,
+            Width = 1
+        };
+        rateLine.AddPoint(new Vector2(-BUCKET_PIXEL_WIDTH * BUCKETS_RIGHT_SIDE_OFFSET, -BUCKET_BOTTOM_OFFSET - 3 * 3 * BUCKET_NOTE_PIXEL_HEIGHT));
+        rateLine.AddPoint(new Vector2(-BUCKET_PIXEL_WIDTH * (_bucketCount + BUCKETS_RIGHT_SIDE_OFFSET), -BUCKET_BOTTOM_OFFSET - 3 * 3 * BUCKET_NOTE_PIXEL_HEIGHT));
+        barNode.AddChild(rateLine);
+        var rateNumber = new Label() {
+            Position = new Vector2(-30, -BUCKET_BOTTOM_OFFSET - 3 * 3 * BUCKET_NOTE_PIXEL_HEIGHT),
+            Text = "3/s"
+        };
+        barNode.AddChild(rateNumber);
+
+        for (var i = 0; i < _bucketCount + BUCKETS_RIGHT_SIDE_OFFSET; i++) {
             // get count of note and count of chords
             var totalCount = noteBuckets[i].Count();
 
             var chordCount = noteBuckets[i].Count(x => x.Notes.Count() > 1);
-            var chordHeight = chordCount*BUCKET_NOTE_PIXEL_HEIGHT;
-            
-            var noteCount = totalCount - chordCount;
-            var noteHeight = noteCount*BUCKET_NOTE_PIXEL_HEIGHT;
+            var chordHeight = chordCount * BUCKET_NOTE_PIXEL_HEIGHT;
 
-            var stringCounts = new float[] { 0,0,0,0,0,0 };
+            var noteCount = totalCount - chordCount;
+            var noteHeight = noteCount * BUCKET_NOTE_PIXEL_HEIGHT;
+
+            var stringCounts = new float[] { 0, 0, 0, 0, 0, 0 };
             foreach (var n in noteBuckets[i]) {
                 foreach (var a in n.Notes) {
                     stringCounts[a.StringNum]++;
                 }
             }
             var stringSum = stringCounts.Sum();
-            
+
             var chord = new ColorRect() {
                 Color = _chordColour,
                 AnchorsPreset = 3,
@@ -105,10 +123,10 @@ public partial class NoteGraph : Node {
                 var newOffset = stringOffsetPos;
                 var y = 0f;
                 if (stringSum > 0) {
-                    y = BUCKET_BOTTOM_OFFSET * stringCounts[x]/stringSum;
+                    y = BUCKET_BOTTOM_OFFSET * stringCounts[x] / stringSum;
                 }
                 stringOffsetPos += y; // for the next one
-                return new ColorRect() { 
+                return new ColorRect() {
                     Color = stringColours[x] * 0.6f,
                     AnchorsPreset = 3,
                     OffsetTop = newOffset,
@@ -116,19 +134,28 @@ public partial class NoteGraph : Node {
                 };
             }).ToArray();
 
-            var g = new GraphSection(new Vector2(-BUCKET_PIXEL_WIDTH*(float)(maxBucket - i), -BUCKET_BOTTOM_OFFSET), chord, note, stringNums);
+            var g = new GraphSection(new Vector2(-BUCKET_PIXEL_WIDTH * (_bucketCount + BUCKETS_RIGHT_SIDE_OFFSET - i), -BUCKET_BOTTOM_OFFSET), chord, note, stringNums);
             _graphBars.Add(i, g);
             barNode.AddChild(g);
         }
-	}
 
-	public override void _Process(double delta)
-	{
+        // setup the current position line
+        _currentPostionLine = new Line2D() {
+            DefaultColor = Colors.Gray,
+            Width = 1
+        };
+        _currentPostionLine.AddPoint(new Vector2(0, 0));
+        _currentPostionLine.AddPoint(new Vector2(0, -3 * 3 * BUCKET_NOTE_PIXEL_HEIGHT - BUCKET_BOTTOM_OFFSET));
+        barNode.AddChild(_currentPostionLine);
+    }
+
+    public override void _Process(double delta) {
         if (!_audio.SongPlaying)
-			return;
+            return;
 
-        var bucket = Math.Round(_audio.GetSongPosition()/BUCKET_SIZE);
+        var bucket = Math.Round(_audio.GetSongPosition() / BUCKET_SIZE);
 
+        // highlight the bar we are in
         foreach (var bar in _graphBars) {
             if (bar.Key == bucket) {
                 bar.Value.Chord.Color = _chordColour * 2;
@@ -138,5 +165,8 @@ public partial class NoteGraph : Node {
                 bar.Value.Note.Color = _noteColour;
             }
         }
-	}
+
+        // move the current position indicator along
+        _currentPostionLine.Position = new Vector2(BUCKET_PIXEL_WIDTH * (-_bucketCount - BUCKETS_RIGHT_SIDE_OFFSET + 0.5f + (float)_audio.GetSongPosition() / BUCKET_SIZE), 0);
+    }
 }
