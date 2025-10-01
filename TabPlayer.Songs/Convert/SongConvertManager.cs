d5b48d58b8b0575b2605b864e015144364256959
murph9 <murph9y@gmail.com>
2025-10-01 17:55:59 +1000

    do not break if album is not pulled

--- TabPlayer.Songs/Convert/SongConvertManager.cs
+++ TabPlayer.Songs/Convert/SongConvertManager.cs
@@ -6,16 +6,14 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace murph9.TabPlayer.Songs.Convert
-{
-    public class SongConvertManager
-    {
+namespace murph9.TabPlayer.Songs.Convert {
+    public class SongConvertManager {
         public enum SongType {
             Psarc, Midi
         }
         public static async Task<DirectoryInfo> ConvertFile(SongType type, string location, bool reconvert, bool copySource, Action<string> output) {
             DirectoryInfo outputFolder = null;
-            
+
             try {
                 if (type == SongType.Midi)
                     outputFolder = ExportMidi(new DirectoryInfo(location), output);
@@ -26,7 +24,7 @@ namespace murph9.TabPlayer.Songs.Convert
                 Console.WriteLine($"Failed to convert file '{location}'");
                 return null;
             }
-            
+
             return outputFolder;
         }
 
@@ -42,9 +40,7 @@ namespace murph9.TabPlayer.Songs.Convert
                     outputFolder = await ExportSinglePsarc(p, null, reconvert, output);
                     if (copySource)
                         File.Copy(file.FullName, Path.Combine(outputFolder.FullName, file.Name), true);
-                }
-                else
-                {
+                } else {
                     string sourceOfSongBatch = null;
 
                     foreach (var name in songNames) {
@@ -67,7 +63,7 @@ namespace murph9.TabPlayer.Songs.Convert
                         }
                     }
                 }
-                
+
             } catch (FileLoadException ef) {
                 output("Failed to convert file, see last error in log");
                 Console.WriteLine(ef);
@@ -82,12 +78,11 @@ namespace murph9.TabPlayer.Songs.Convert
                 Console.WriteLine($"Converted {file.FullName}");
                 p?.Dispose();
             }
-            
+
             return outputFolder;
         }
 
-        private static async Task<DirectoryInfo> ExportSinglePsarc(PsarcFile p, string songFilter, bool reconvert, Action<string> output)
-        {
+        private static async Task<DirectoryInfo> ExportSinglePsarc(PsarcFile p, string songFilter, bool reconvert, Action<string> output) {
             var converter = new PsarcFileConverter(p, songFilter);
             var noteInfo = converter.ConvertToSongInfo();
 
@@ -100,7 +95,7 @@ namespace murph9.TabPlayer.Songs.Convert
             }
             foreach (var f in outputFolder.GetFiles("*.json"))
                 f.Delete();
-            
+
             await WriteSongInfo(noteInfo, outputFolder.FullName, "data");
 
             output("Writing audio to " + outputFolder.FullName);
@@ -112,18 +107,22 @@ namespace murph9.TabPlayer.Songs.Convert
             foreach (var f in new DirectoryInfo(outputFolder.FullName).GetFiles("*.wem"))
                 f.Delete();
 
-            converter.ExportAlbumArt(outputFolder.FullName);
+            try {
+                converter.ExportAlbumArt(outputFolder.FullName);
+            } catch (ArgumentException e) {
+                Console.WriteLine("Failed to save image art: " + e);
+            }
 
             output("Wrote " + noteInfo?.Metadata?.Name + " info to " + outputFolder.FullName);
 
             return outputFolder;
-            
+
         }
 
         private static async Task<string> WriteSongInfo(SongInfo info, string tempFolder, string dataFileName) {
             var jsonFile = Path.Join(tempFolder, dataFileName + ".json");
-            await File.WriteAllTextAsync(jsonFile, JsonConvert.SerializeObject(info, Newtonsoft.Json.Formatting.None, 
-                        new JsonSerializerSettings { 
+            await File.WriteAllTextAsync(jsonFile, JsonConvert.SerializeObject(info, Newtonsoft.Json.Formatting.None,
+                        new JsonSerializerSettings {
                             NullValueHandling = NullValueHandling.Ignore,
                             DefaultValueHandling = DefaultValueHandling.Ignore
                         }));
