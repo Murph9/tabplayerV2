e78151a35636940b9faab8ce3d501db2285c1339
murph9 <murph9y@gmail.com>
2025-10-06 11:50:19 +1100

    update godot to 4.5 + fix image issues

--- godot/scenes/SongDisplay.cs
+++ godot/scenes/SongDisplay.cs
@@ -7,62 +7,71 @@ using System.Linq;
 
 namespace murph9.TabPlayer.scenes;
 
-public partial class SongDisplay : VBoxContainer
-{
-	[Signal]
-	public delegate void SongSelectedEventHandler(string folder, string instrument);
+public partial class SongDisplay : VBoxContainer {
+    [Signal]
+    public delegate void SongSelectedEventHandler(string folder, string instrument);
 
-	private string _folderName;
+    private string _folderName;
 
-	public override void _Ready() {}
+    public override void _Ready() { }
 
-	public override void _Process(double delta) { }
+    public override void _Process(double delta) { }
 
-	public void SongChanged(string folderName) {
-		_folderName = folderName;
-		LoadSong();
-	}
+    public void SongChanged(string folderName) {
+        _folderName = folderName;
+        LoadSong();
+    }
 
-	private void LoadSong() {
+    private void LoadSong() {
 
-		var songInfo = SongLoader.Load(_folderName, null).SongInfo;
+        var songInfo = SongLoader.Load(_folderName, null).SongInfo;
 
-		var image = Image.LoadFromFile(Path.Combine(SongFileManager.SONG_FOLDER, _folderName, SongFileManager.ALBUM_ART_NAME));
-		GetNode<TextureRect>("AlbumArtTextureRect").Texture = ImageTexture.CreateFromImage(image);
+        var imageFile = Path.Combine(SongFileManager.SONG_FOLDER, _folderName, SongFileManager.ALBUM_ART_NAME);
+        Texture2D albumImage = new GradientTexture2D() {
+            Gradient = new Gradient() {
+                Colors = [Colors.Transparent]
+            }
+        };
+        if (File.Exists(imageFile)) {
+            var image = Image.LoadFromFile(imageFile);
+            if (image is not null) {
+                albumImage = ImageTexture.CreateFromImage(image);
+            }
+        }
+        GetNode<TextureRect>("AlbumArtTextureRect").Texture = albumImage;
 
-		GetNode<Label>("ArtistLabel").Text = "Artist: " + songInfo.Metadata.Artist;
-		GetNode<Label>("SongNameLabel").Text = "Name: " + songInfo.Metadata.Name;
-		GetNode<Label>("AlbumLabel").Text = "Album: " + songInfo.Metadata.Album;
-		GetNode<Label>("YearLabel").Text = "Year: " + songInfo.Metadata.Year.ToString();
-		GetNode<Label>("OtherLabel").Text = "Length: " + songInfo.Metadata.SongLength.ToMinSec();
+        GetNode<Label>("ArtistLabel").Text = "Artist: " + songInfo.Metadata.Artist;
+        GetNode<Label>("SongNameLabel").Text = "Name: " + songInfo.Metadata.Name;
+        GetNode<Label>("AlbumLabel").Text = "Album: " + songInfo.Metadata.Album;
+        GetNode<Label>("YearLabel").Text = "Year: " + songInfo.Metadata.Year.ToString();
+        GetNode<Label>("OtherLabel").Text = "Length: " + songInfo.Metadata.SongLength.ToMinSec();
 
-		var grid = GetNode<GridContainer>("InstrumentGridContainer");
-		grid.GetChildren().ToList().ForEach(grid.RemoveChild); // remove all children from the grid
+        var grid = GetNode<GridContainer>("InstrumentGridContainer");
+        grid.GetChildren().ToList().ForEach(grid.RemoveChild); // remove all children from the grid
 
-		grid.AddChild(new Label());
-		grid.AddChild(new Label() { Text = "Tuning"});
-		grid.AddChild(new Label() { Text = "Note Counts"});
-		grid.AddChild(new Label() { Text = "Note Density"});
-		
-		grid.Columns = grid.GetChildren().Count;
+        grid.AddChild(new Label());
+        grid.AddChild(new Label() { Text = "Tuning" });
+        grid.AddChild(new Label() { Text = "Note Counts" });
+        grid.AddChild(new Label() { Text = "Note Density" });
 
-		var insturmentsOrdered = songInfo.Instruments.OrderBy(x => 
-			{
-				if (SongInfo.INSTRUMENT_ORDER.ContainsKey(x.Name))
-					return SongInfo.INSTRUMENT_ORDER[x.Name];
-				// handle all other entries as in given order
-				return 999;
-			}).ToList();
+        grid.Columns = grid.GetChildren().Count;
 
-		foreach (var i in insturmentsOrdered) {
-			var button = new Button() { Text = "Play " + i.Name.Capitalize() };
-			button.Pressed += () => {
-				EmitSignal(SignalName.SongSelected, _folderName, i.Name);
-			};
-			grid.AddChild(button);
-			grid.AddChild(new Label() { Text = Instrument.CalcTuningName(i.Config.Tuning, i.Config.CapoFret) });
-			grid.AddChild(new Label() { Text = $"{i.TotalNoteCount()}, c: {i.ChordCount()}, n: {i.SingleNoteCount()}" });
-			grid.AddChild(new Label() { Text = i.GetNoteDensity(songInfo).ToFixedPlaces(2) });
-		}
-	}
+        var insturmentsOrdered = songInfo.Instruments.OrderBy(x => {
+            if (SongInfo.INSTRUMENT_ORDER.ContainsKey(x.Name))
+                return SongInfo.INSTRUMENT_ORDER[x.Name];
+            // handle all other entries as in given order
+            return 999;
+        }).ToList();
+
+        foreach (var i in insturmentsOrdered) {
+            var button = new Button() { Text = "Play " + i.Name.Capitalize() };
+            button.Pressed += () => {
+                EmitSignal(SignalName.SongSelected, _folderName, i.Name);
+            };
+            grid.AddChild(button);
+            grid.AddChild(new Label() { Text = Instrument.CalcTuningName(i.Config.Tuning, i.Config.CapoFret) });
+            grid.AddChild(new Label() { Text = $"{i.TotalNoteCount()}, c: {i.ChordCount()}, n: {i.SingleNoteCount()}" });
+            grid.AddChild(new Label() { Text = i.GetNoteDensity(songInfo).ToFixedPlaces(2) });
+        }
+    }
 }
